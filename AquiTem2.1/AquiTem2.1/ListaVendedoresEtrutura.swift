7564b5bff1400d6af85416c80324924303c3a674
MayaraCoelho <mayaracoelho.nave@gmail.com>
2015-03-22 15:18:01 -0300

    Mayara Coelho
    Commit com o início da construção da TableView

---
+++ AquiTem2.1/AquiTem2.1/ListaVendedoresEtrutura.swift
@@ -0,0 +1,19 @@
+//
+//  ListaVendedoresEtrutura.swift
+//  AquiTem2.1
+//
+//  Created by Mayara Coelho on 3/22/15.
+//  Copyright (c) 2015 Mayara Coelho. All rights reserved.
+//
+
+import Foundation
+
+// classe responsável pela estrutura da TableViewController
+struct ListaVendedoresEtrutura {
+    
+    //variáveis responsáveis pelo conteúdo que aparecerá dentro de cada células.
+    var nome: String
+    var marca: String
+    
+
+}
